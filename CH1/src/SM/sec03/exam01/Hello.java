1acbaf88a2c277d7e88caa1fbc644204f228ab01
Eddie <94687817+seoi0698@users.noreply.github.com>
2024-09-21 14:52:31 +0900

    chapter01

---
+++ CH1/src/SM/sec03/exam01/Hello.java
@@ -0,0 +1,14 @@
+package SM.sec03.exam01;
+
+public class Hello {
+/*
+ * write : min seon
+ * date of creation : 20240920
+ */
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
