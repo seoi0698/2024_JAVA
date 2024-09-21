5c7302992c7d97b382d6cbb51c8920e2e6dd72c0
Eddie <94687817+seoi0698@users.noreply.github.com>
2024-09-21 19:57:01 +0900

    smChapter02

---
+++ CH2/src/SM/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+package SM.sec01.exam01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		int value;
+		int result = value + 10;
+		System.out.println("result");		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CH2/src/SM/sec01/exam02/VariableUseExample.java
@@ -0,0 +1,17 @@
+package SM.sec01.exam02;
+
+public class VariableUseExample {
+
+	public static void main(String[] args) {
+		int hour = 3;
+		int minute = 5;
+		System.out.println(hour + "hour " + minute + "minute");
+		
+		int totalMinute= (hour*60) + minute;
+		System.out.println("total" + totalMinute + "minute");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
