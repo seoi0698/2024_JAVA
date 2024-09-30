350b09987fe4e0e49d25bf760f2a17b618d5ba71
Eddie <94687817+seoi0698@users.noreply.github.com>
2024-09-30 20:29:11 +0900

    CH3

---
+++ CH3/src/SM/sec03/exam03/AtirhmeticOperatorExample.java
@@ -0,0 +1,15 @@
+package SM.sec03.exam03;
+
+public class AtirhmeticOperatorExample {
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+
+		play = !play;
+		System.out.println(play);
+
+		play = !play;
+		System.out.println(play);
+	}
+
+}
